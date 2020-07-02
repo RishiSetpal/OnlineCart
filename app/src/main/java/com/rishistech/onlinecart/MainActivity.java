2b385c4e409e66b7f707c90a3a53275e47dee962
RishiSetpal <rishisetpal123@gmail.com>
2020-07-02 17:14:18 +0530

    SplashScreen Commit..

--- app/src/main/java/com/rishistech/onlinecart/MainActivity.java
+++ app/src/main/java/com/rishistech/onlinecart/MainActivity.java
@@ -1,7 +1,6 @@
 package com.rishistech.onlinecart;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
---
+++ app/src/main/java/com/rishistech/onlinecart/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.rishistech.onlinecart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        SystemClock.sleep(3000);
+        Intent i = new Intent(SplashActivity.this,MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
