f2500bbd6e366207ea3aaa0f848fab6a77f7b8c1
RishiSetpal <rishisetpal123@gmail.com>
2020-07-05 00:54:38 +0530

    homee>Login With Profile Commit..

--- app/src/main/java/com/rishistech/onlinecart/HomePage.java
+++ app/src/main/java/com/rishistech/onlinecart/HomePage.java
@@ -2,18 +2,31 @@ package com.rishistech.onlinecart;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.core.view.GravityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -27,11 +40,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class HomePage extends AppCompatActivity
-    implements NavigationView.OnNavigationItemSelectedListener{
+        implements NavigationView.OnNavigationItemSelectedListener, DrawerLayout.DrawerListener {
 
     private AppBarConfiguration mAppBarConfiguration;
     private FrameLayout frameLayout;
-    private FirebaseAuth firebaseAuth;
+    //
+    private ImageView home_profile_image;
+    private TextView home_profile_name;
+    private TextView home_profile_email;
+
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+    private FirebaseUser user;
+    private String id;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +72,45 @@ public class HomePage extends AppCompatActivity
         frameLayout = findViewById(R.id.main_framelayout);
         setFragment((new HomeeFragment()));
 
+        ///// Profile
+
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("users");
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            id = user.getUid();
+        }
+
+        //NavigationView navigationView = (NavigationView) findViewById(R.id.add_profile_icon);
+        View headerView = navigationView.getHeaderView(0);
+        home_profile_image = (ImageView) headerView.findViewById(R.id.main_profile_image);
+        home_profile_name = (TextView) headerView.findViewById(R.id.main_fullname);
+        home_profile_email = (TextView) headerView.findViewById(R.id.main_email);
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()){
+                    if (Objects.equals(ds.child("id").getValue(String.class), id)){
+                        home_profile_name.setText(ds.child("name").getValue(String.class));
+                        home_profile_email.setText(ds.child("email").getValue(String.class));
+
+                        Glide.with(HomePage.this).load(ds.child("photourl").getValue(String.class)).into(home_profile_image);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(HomePage.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        /////Profile
+
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
 //        mAppBarConfiguration = new AppBarConfiguration.Builder(
@@ -59,6 +120,7 @@ public class HomePage extends AppCompatActivity
 //        NavController navController = Navigation.findNavController(this, R.id.nav_home);
 //        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
 //        NavigationUI.setupWithNavController(navigationView, navController);
+
     }
 
     @Override
@@ -121,4 +183,23 @@ public class HomePage extends AppCompatActivity
     }
 
 
+    @Override
+    public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+
+    }
+
+    @Override
+    public void onDrawerOpened(@NonNull View drawerView) {
+
+    }
+
+    @Override
+    public void onDrawerClosed(@NonNull View drawerView) {
+
+    }
+
+    @Override
+    public void onDrawerStateChanged(int newState) {
+
+    }
 }
